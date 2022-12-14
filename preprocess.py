feb7f37959d9e0083e44907114922a3bb4fd77b2
Farah-Boun <gf_bounsiar@esi.dz>
2022-12-14 19:41:34 +0100

    Add Data generator with normalisation function

--- preprocess.py
+++ preprocess.py
@@ -1,1 +1,11 @@
 #Part of of the code that will preprocess images before training
+from keras.preprocessing.image import ImageDataGenerator
+
+
+def dataGen (center = False, nomalize = False):
+    datagen = ImageDataGenerator(
+          featurewise_center=center,              
+          featurewise_std_normalization=nomalize)
+    return datagen
+
+
