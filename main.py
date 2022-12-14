89dcf4d95c6dea8c888825e856f0e75d604341dc
MehdiBoudjelli <ha_boudjelli@esi.dz>
2022-12-14 18:40:00 +0100

    Create CNN

--- main.py
+++ main.py
@@ -5,9 +5,41 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 from keras.datasets import mnist
 
+from keras.models import Model, Sequential
+from keras.models import load_model
+from keras.layers import Input, Conv2D, MaxPooling2D, BatchNormalization
+from keras.layers import UpSampling2D, Dropout, Dense, Flatten
 
 #Chargement des données
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-plt.imshow(x_train[0],cmap="gray")
-plt.show()
+
+
+#Création du CNN
+model = Sequential()
+
+model.add(Conv2D(filters = 32, kernel_size = (5, 5), activation='relu', padding='Same', input_shape = (28, 28, 1)))
+model.add(BatchNormalization())
+model.add(Conv2D(filters = 32, kernel_size = (5, 5), activation='relu', padding='Same'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(strides=(2,2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(filters = 64, kernel_size = (3, 3), activation='relu'))
+model.add(BatchNormalization())
+model.add(Conv2D(filters = 64, kernel_size = (3, 3), activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(strides=(2,2)))
+model.add(Dropout(0.25))
+
+#Couche de classification entièrement connectée
+model.add(Flatten())     
+model.add(Dense(units=1024, activation='relu'))
+model.add(Dropout(0.25))
+model.add(Dense(units=1024, activation='relu'))
+model.add(Dropout(0.25))
+
+#Couche de prédicion
+model.add(Dense(units=10, activation='softmax'))
+
+model.summary()
