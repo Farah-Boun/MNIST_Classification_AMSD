2cdec001a515e48c25040ad89339834753244d74
Farah-Boun <gf_bounsiar@esi.dz>
2023-01-05 22:34:16 +0100

    Added speckle noise and a noise selector fct

--- preprocess.py
+++ preprocess.py
@@ -2,25 +2,33 @@
 from keras.preprocessing.image import ImageDataGenerator
 import numpy as np
 
+def select_noise(noise_type = "gaussian"):
+      if noise_type=="gaussian":
+            return add_gaussian_noise
+      if noise_type=="speckle":
+            return add_speckle_noise
 
-def add_noise(img,noise_type="gaussian",probability=0.5,mean=0,sigma=10):
+def add_gaussian_noise(img,probability=0.5,mean=0,sigma=10):
       row =  img.shape[0]
       col =  img.shape[1]
       img=img.astype(np.float32)
-      if noise_type=="gaussian":
-            noise=np.random.normal(mean,sigma,img.shape)
-            noise=noise.reshape(row,col)
-            for i in range(row):
-                  for j in range(col):
-                        if(np.random.uniform(0,1)<probability):
-                              img[i,j]=img[i,j]+noise[i,j]
-
-
-      if noise_type=="speckle":
-            noise=np.random.randn(row,col)
-            noise=noise.reshape(row,col)
-            img=img+img*noise
+      noise=np.random.normal(mean,sigma,img.shape)
+      noise=noise.reshape(row,col)
+      for i in range(row):
+            for j in range(col):
+                  if(np.random.uniform(0,1)<probability):
+                        img[i,j]=img[i,j]+noise[i,j]
       
+      return img
+
+
+def add_speckle_noise(img,probability=0.5,mean=0,sigma=10):
+      row =  img.shape[0]
+      col =  img.shape[1]
+      img=img.astype(np.float32)
+      noise=np.random.randn(row,col)
+      noise=noise.reshape(row,col,1)
+      img=img+img*noise
       return img
 
 def dataGen (center = False, nomalize = False , noise_type = "gaussian" ):
@@ -29,7 +37,7 @@ def dataGen (center = False, nomalize = False , noise_type = "gaussian" ):
           zoom_range = 0.2,                     
           width_shift_range=0.1,               
           height_shift_range=0.1,
-          preprocessing_function=add_noise
+          preprocessing_function=select_noise(noise_type)
           )    
     return datagen
 
