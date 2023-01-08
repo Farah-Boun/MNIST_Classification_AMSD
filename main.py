a70fd47599752d7559b673af4d4721c1304d0731
MehdiDeb <hm_debiane@esi.dz>
2023-01-08 22:17:00 +0100

    update training

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
-import matplotlib.pyplot as plt       
 import numpy as np                    
-import pandas as pd                   
-#from sklearn.model_selection import train_test_split
-#from sklearn.metrics import confusion_matrix
 import keras
 from keras.datasets import mnist
-import tensorflow as tf
-from keras.models import Model, Sequential
-from keras.models import load_model
-from keras.layers import Input, Conv2D, MaxPooling2D, BatchNormalization
-from keras.layers import UpSampling2D, Dropout, Dense, Flatten
-from keras.optimizers import Adam
-from keras import layers
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, BatchNormalization
+from keras.layers import Dropout, Dense, Flatten
 from tqdm import tqdm
 
 from preprocess import dataGen 
@@ -19,6 +11,27 @@ from preprocess import dataGen
 
 #Chargement des données
 (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
+
+def add_noise(img,noise_type="gaussian",probability=0.5,mean=0,sigma=10):
+  
+  row,col=img.shape
+  img=img.astype(np.float32)
+  
+  if noise_type=="gaussian":
+    noise=np.random.normal(mean,sigma,img.shape)
+    noise=noise.reshape(row,col)
+    for i in range(row):
+      for j in range(col):
+        if(np.random.uniform(0,1)<probability):
+          img[i,j]=img[i,j]+noise[i,j]
+    return img
+
+  if noise_type=="speckle":
+    noise=np.random.randn(row,col)
+    noise=noise.reshape(row,col)
+    img=img+img*noise
+    
+    return img
 
 
 def get_cnn():
@@ -39,72 +52,60 @@ def get_cnn():
     model.add(MaxPooling2D(strides=(2,2)))
     model.add(Dropout(0.25))
 
-    #Couche de classification entièrement connectée
     model.add(Flatten())     
     model.add(Dense(units=1024, activation='relu'))
     model.add(Dropout(0.25))
     model.add(Dense(units=1024, activation='relu'))
     model.add(Dropout(0.25))
 
-    #Couche de prédicion
     model.add(Dense(units=10, activation='softmax'))
 
     return model
 
 
 def get_autoencoder():
+    
+    #Création de l'autoencoder 
 
     model = Sequential()
-    model.add(Flatten(input_shape=(28, 28, 1), name="layer0"))     
-    #model.add(Dense(units=784, activation='relu'))
-    model.add(Dense(units=256, activation='relu', name="layer1"))
-    model.add(Dense(units=128, activation='relu', name="layer2"))
-    model.add(Dense(units=64, activation='relu', name="layer3"))
+    model.add(Flatten(input_shape=(28, 28, 1)))    
+    model.add(Dense(units=512, activation='relu'))
+    model.add(Dense(units=256, activation='relu'))
     model.add(Dense(units=128, activation='relu'))
     model.add(Dense(units=256, activation='relu'))
+    model.add(Dense(units=512, activation='relu')) 
     model.add(Dense(units=784, activation='sigmoid'))
-    #model.build((None, 28, 28, 1))
 
     return model
 
-def get_encoder(autoencoder):
-  model = Sequential()
-  for i in range(0,4):
-    model.add(autoencoder.layers[i])
-  return model
+def get_classifier(autoencoder): 
 
-def get_classifier(autoencoder):
+  #Création du classifier basé sur un encoder suivi d'une couche dense entièrement connectée
   model = Sequential()
   for i in range(4):
     model.add(autoencoder.layers[i])
-  model.add(Dense(units=1000, activation='relu', name="perc"))
+  model.add(Dense(units=5000, activation='relu', name="perc"))
   model.add(Dropout(0.5))
   model.add(Dense(units=10, activation='softmax'))
   for i in range(0,4):
     model.layers[i].trainable = False
-  """for layer in model.layers:
-    print(layer.trainable)
-  model.summary()"""
   return model
-
-def get_train_gaussian():
-  train = np.load('traindata_gauss.npy')
-  return train
-
-def get_train_speckle():
-  train = np.load('traindata_speckle.npy')
-  return train
 
 
 def train_autoencoder(optimizer, epochs, lr, steps):
+
+  # Fonction responsable de l'entrainement de l'autoencoder selon les paramètres choisis par l'utilisateur
   (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
   autoencoder = get_autoencoder()
-  traindata_gauss = get_train_gaussian()
-  traindata_speckle = get_train_speckle()
+  traindata_gauss=np.zeros((60000,28,28))
+  for i in tqdm(range(len(X_train))):
+    traindata_gauss[i]=add_noise(X_train[i],noise_type="gaussian")
+  traindata_speckle=np.zeros((60000,28,28))
+  for i in tqdm(range(len(X_train))):
+    traindata_speckle[i]=add_noise(X_train[i],noise_type="speckle")
   X=np.concatenate((X_train,traindata_gauss, traindata_speckle), axis=0)
   X2=np.concatenate((X_train,X_train, X_train), axis=0)
   X_train2 =X.reshape(60000*3, 28, 28, 1)/255
-  print("x train : ", X_train.shape)
   X_train3 = X2.reshape(60000*3, 784)/255
 
   if optimizer:
@@ -113,16 +114,17 @@ def train_autoencoder(optimizer, epochs, lr, steps):
     opt = keras.optimizers.Adam(learning_rate=lr)
 
   autoencoder.compile(optimizer=opt, loss='binary_crossentropy', metrics=["accuracy"])
-  print("start")
   hist = autoencoder.fit_generator(dataGen().flow(X_train2, X_train3),
-                            steps_per_epoch=steps,             # nombre image entrainement / batch_size
-                            epochs=epochs,                        # nombre de boucle à réaliser sur le jeu de données complet
-                            verbose=1,                        # verbosité
-                            #validation_data=(X_test, X_test)
+                            steps_per_epoch=steps,            
+                            epochs=epochs,                       
+                            verbose=1,                        
                             )
   return autoencoder
 
 def train_classifier(autoencoder, optimizer, epochs, lr, steps):
+
+  # Fonction responsable de l'entrainement du classifier basé sur l'encoder selon les paramètres choisis par l'utilisateur
+  
   (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
   
   model = get_classifier(autoencoder)
@@ -130,7 +132,7 @@ def train_classifier(autoencoder, optimizer, epochs, lr, steps):
   Y_train = keras.utils.to_categorical(Y_train, 10)
   Y_test = keras.utils.to_categorical(Y_test, 10)
 
-  X_train=X_train.reshape(60000, 28, 28, 1)
+  X_train=X_train.reshape(60000, 28, 28, 1)/255
 
   if optimizer:
     opt = keras.optimizers.SGD(learning_rate=lr)
@@ -140,14 +142,16 @@ def train_classifier(autoencoder, optimizer, epochs, lr, steps):
   model.compile(loss='categorical_crossentropy', optimizer = opt, metrics=["accuracy"])
 
   hist = model.fit_generator(dataGen().flow(X_train, Y_train, batch_size=32),
-                            steps_per_epoch=steps,             # nombre image entrainement / batch_size
-                            epochs=epochs,                        # nombre de boucle à réaliser sur le jeu de données complet
-                            verbose=1,                        # verbosité
+                            steps_per_epoch=steps,             
+                            epochs=epochs,                       
+                            verbose=1,                       
                             )
 
   return model
 
 def train_cnn(optimizer, epochs, lr, steps):
+
+  # Fonction responsable de l'entrainement du CNN selon les paramètres choisis par l'utilisateur
 
   (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
   model = get_cnn()
@@ -165,9 +169,9 @@ def train_cnn(optimizer, epochs, lr, steps):
   model.compile(loss='categorical_crossentropy', optimizer = opt, metrics=["accuracy"])
 
   hist = model.fit_generator(dataGen().flow(X_train, Y_train, batch_size=32),
-                            steps_per_epoch=steps,             # nombre image entrainement / batch_size
-                            epochs=epochs,                        # nombre de boucle à réaliser sur le jeu de données complet
-                            verbose=1,                        # verbosité
+                            steps_per_epoch=steps,           
+                            epochs=epochs,                       
+                            verbose=1,                      
                             )
   return model
 
@@ -181,23 +185,3 @@ def train(optimizer, epochs, lr, steps, ae_optimizer, ae_epochs, ae_lr, ae_steps
     print("error classifier type")
 
   return model 
-
-
-
-
-Y_test = keras.utils.to_categorical(Y_test, 10)
-
-mt = "encoder"
-
-if mt=="cnn":
-  #model = train_cnn(0, 5, 0.001, 1000)
-  model = train(0, 5, 0.001, 20, 0, 25, 0.001, 1000)
-elif mt=="encoder":
-  auto = train_autoencoder(0, 25, 0.001, 1000)
-  model = train_classifier(auto,0, 25, 0.001, 1000)
-else:
-  print("error classifier type")
-
-final_loss, final_acc = model.evaluate(X_test, Y_test, verbose=0)
-
-print("Final loss: {0:.4f}, final accuracy: {1:.4f}".format(final_loss, final_acc))
